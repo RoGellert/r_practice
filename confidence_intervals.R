82233f7dd5444e3e1098f07fa349475cc6c14fb6
Roman Gellert <titangtace@gmail.com>
2023-11-25 17:24:37 +0100

    Solutions

---
+++ confidence_intervals.R
@@ -0,0 +1,34 @@
+#exercise1
+
+qt(0.95, df=11)
+qt(0.975, df=6)
+qt(0.995, df=1)
+qt(0.975, df=28)
+
+#exercise3
+
+1-2*pt(-2.776, df=4)
+1-2*pt(-2.718, df=11)
+1-2*pt(-5.841, df=3)
+1-2*pt(-1.325, df=20)
+1-2*pt(-1.746, df=16)
+
+#exercise5
+
+data <- c(3.32,2.53,3.45,2.38,3.01)
+mu <- mean(data)
+sd <- sd(data)
+n <- length(data)
+cf <- c(mu-(qt(0.975, df=n-1)*sd)/sqrt(n), mu+(qt(0.975, df=n-1)*sd)/sqrt(n))
+
+#exercise7
+
+data<-c(204.999,206.149,202.102,207.048,203.496,206.343,203.496,206.676,205.831)
+mu <- mean(data)
+sd <- sd(data)
+n <- length(data)
+cf <- c(mu-(qt(0.975, df=n-1)*sd)/sqrt(n), mu+(qt(0.975, df=n-1)*sd)/sqrt(n))
+
+#exercise9
+
+
